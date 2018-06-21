a3e69816699df6b4d22e27aa2cc7f4c211102945
sunshiyuan <shiyuan.sun@trustway.cn>
2018-06-21 11:21:17 +0800

    修改导航

--- ceshi/ViewController.m
+++ ceshi/ViewController.m
@@ -19,6 +19,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    
+    
     SonModel *son = [[SonModel alloc]init];
     NSString *str = @"0.55";
     float a = [str floatValue];
